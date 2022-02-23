1d8b3e65f13d28074806b83440fb15e5a837ad7c
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-02-23 09:50:00 -0300

    .

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns')
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +20,11 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl/:url', function(req,res){
+  const url = req.body
+  res.json({ original_url: url, short_url: '' })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
